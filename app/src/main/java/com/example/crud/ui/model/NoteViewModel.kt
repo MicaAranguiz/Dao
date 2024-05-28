3cfcdde6a4032f40c4babbbc6db88cbf0a9be3df
micaaranguiz <micaelaaranguiz2@gmail.com>
2024-05-28 20:47:48 -0300

    Version del 14-05 nuevo

--- app/src/main/java/com/example/crud/ui/model/NoteViewModel.kt
+++ app/src/main/java/com/example/crud/ui/model/NoteViewModel.kt
@@ -2,7 +2,9 @@ package com.example.crud.ui.model
 
 import android.app.Application
 import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.example.crud.core.TextFieldState
@@ -10,13 +12,24 @@ import com.example.crud.db.Model.Note
 import com.example.crud.db.NotesDatabase
 import com.example.crud.repository.NotesRepository
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
+import java.util.Date
 
+
+// buscar todos los metodos y entender que están llamando
 class NoteViewModel(application: Application) : ViewModel() {
 
     private val _text = mutableStateOf(TextFieldState())
     val text: State<TextFieldState> = _text
     private var currentId: Int? = null
+    var openDialog by mutableStateOf(false)
+
+    private val _eventFlow = MutableSharedFlow<Event>()
+    val eventFlow = _eventFlow.asSharedFlow()
 
     private var repository: NotesRepository
         get() {
@@ -36,6 +49,7 @@ class NoteViewModel(application: Application) : ViewModel() {
         all = repository.getNotes()
     }
 
+
     private fun load(id: Int?) {
         viewModelScope.launch {
             //solamente cuando el foco esté sobre la nota que lo ejecute, sino no
@@ -52,6 +66,42 @@ class NoteViewModel(application: Application) : ViewModel() {
                 _text.value = text.value.copy(
                     text = "text"
                 )
+            }
+        }
+    }
+    fun onEvent(event: Event){
+        when(event){
+            is Event.setText ->{
+                _text.value = text.value.copy(
+                    text = event.text
+                )
+            }
+            is Event.Save -> {
+                if (currentId != null){
+                    repository.update(Note(currentId, text.value.text, Date()))
+                }
+                else{
+                    repository.insert(Note(null, text.value.text, Date()))
+                }
+
+                openDialog = false
+                coroutineScope.launch(Dispatchers.IO){
+                    _eventFlow.emit(Event.save)
+                }
+            }
+
+            is Event.OpenDialog -> {
+                openDialog = true
+            }
+            is Event.CloseDialog -> {
+                openDialog = false
+            }
+            is Event.Load -> {
+                load(event.id)
+                openDialog = true
+            }
+            is Event.Delete -> {
+                event.id?.let {repository.delete(it)}
             }
         }
     }
